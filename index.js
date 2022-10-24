0532bab999763038b14a5af3efbe5d342c7ee7ef
dd1sv <dd1svm@yandex.ru>
2022-10-25 01:08:28 +0300

    ready tuples

---
+++ index.js
@@ -0,0 +1,6 @@
+// arrays
+var skills = ['Dev', 'DevOps'];
+for (var _i = 0, skills_1 = skills; _i < skills_1.length; _i++) {
+    var skill = skills_1[_i];
+    console.log(skill);
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 // arrays
 const skills: string[] = ['Dev', 'DevOps']
+for(const skill of skills) {
+    console.log(skill)
+}
+// Tuples
+const skillsTwo: [string, number] = ['Dev', 1]
