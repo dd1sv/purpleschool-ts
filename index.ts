09b8582e3dd9c21d5a4a44b8412acbef17813ba8
dd1sv <dd1svm@yandex.ru>
2022-10-26 01:05:58 +0300

    ready enums sudy

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 // arrays
 const skills: string[] = ['Dev', 'DevOps']
+const skillsEx: Array<string> = ['Dev', 'DevOps']
 for(const skill of skills) {
     console.log(skill)
 }
-// Tuples
+// Tuples - кортежи
 const skillsTwo: [string, number] = ['Dev', 1]
+
+// readonly
+const skillsThree: readonly [number, string] = [1, 'Dev']
+
+// enums
+
+enum StatusCode {
+    SUCCESS,
+    IN_PROCESS,
+    FALIED
+}
+const res = {
+    message: 'Платёж ок',
+    statusCode: StatusCode.SUCCESS
+}
+if (res.statusCode === StatusCode.SUCCESS){
+    console.log('good')
+}
+
