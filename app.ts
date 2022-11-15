16a1f33876a19bbe9f7908637ee7b024e1fff27c
dd1sv <dd1svm@yandex.ru>
2022-11-16 00:09:35 +0300

    union

--- app.ts
+++ app.ts
@@ -1,1 +1,29 @@
-// Продвинутыеsadasvdf
+// Продвинутые
+// union type
+const arr = ['sdf', 1]
+
+function logId(id: string | number | boolean) {
+    if (typeof id === 'string') {
+        console.log(id)
+    } else if (typeof id === 'number') {
+        console.log(id)
+    } else {
+        console.log(id)
+    }
+} 
+
+function logError(err: string | string[]){
+    if (Array.isArray(err)) {
+        console.log(err)
+    } else {
+        console.log(err)
+    }
+}
+
+function logObj(obj : {a: number} | {b :string[]}){
+    if ('a' in obj)) {
+        console.log(err)
+    } else {
+        console.log(err)
+    }
+}
